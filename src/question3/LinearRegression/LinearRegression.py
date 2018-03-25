47f9da45757e448828d82e985f2279554bc37d32
davdevor <drdcustomlows@gmail.com>
2018-03-25 16:54:18 -0400

    added a method to use gradient descent

--- src/question3/LinearRegression/LinearRegression.py
+++ src/question3/LinearRegression/LinearRegression.py
@@ -13,13 +13,64 @@ def read_data(filename,attributes):
             temp_dict[y] = x[y]
         data.append(temp_dict)
         temp_dict = {}
+    file.close()
     return data
 
 
 def linear_interpolation(x0,x,x1,y0,y1):
     return (y0*(x1-x) + y1*(x-x0)) / (x1 - x0)
 
-    
+
+def gradient_descent():
+    attributes = ['Pclass', 'Sex', 'Age', 'Fare', 'Survived']
+    data = read_data('../titanic_data.csv', attributes)
+    for x in data:
+        x['Sex'] = 0.0 if x['Sex'] == 'Male' else 1.0
+        x['Age'] = 10.0 if x['Age'] == '' else float(x['Age'])
+    for x in data:
+        for y in attributes:
+            x[y] = float(x[y])
+    attributes.remove('Survived')
+    x_list = []
+    y_list = []
+
+    clf = linear_model.SGDClassifier(alpha=0.0001, average=False, class_weight=None, epsilon=0.1,
+                                     eta0=0.0, fit_intercept=True, l1_ratio=0.15,
+                                     learning_rate='optimal', loss='huber', max_iter=100000,
+                                     n_jobs=1, penalty='l2', power_t=0.5, random_state=None,
+                                     shuffle=True, tol=None, verbose=0, warm_start=False)
+    for x in range(int(len(data) * .7)):
+        y_list.append(float(data[x].get('Survived')))
+        temp_list = []
+        for y in attributes:
+            temp_list.append(data[x].get(y))
+        x_list.append(temp_list)
+        temp_list = []
+    clf.fit(x_list, y_list)
+    print(clf.coef_)
+
+    test_list = []
+    test_answers = []
+    thirty_percent = int(len(data) * .3)
+    # seventy_percent = int(len(data)*.7)
+    for x in range(len(data) - 1, len(data) - thirty_percent, -1):
+        temp_list = []
+        test_answers.append(int(data[x]['Survived']))
+        for y in attributes:
+            temp_list.append(data[x].get(y))
+        test_list.append(temp_list)
+        temp_list = []
+    predict = clf.predict(test_list)
+    count = 0
+    print('[Pclass, Sex, Age, Fare]')
+    for x in range(len(predict)):
+        rounded = int(round(predict[x]))
+        temp_string = str(test_list[x])
+        print(temp_string + " actual " + str(test_answers[x]) + " predicted " + str(rounded))
+        if rounded == test_answers[x]:
+            count += 1
+    print("Percent correct " + str(count / thirty_percent))
+
 def regression():
     attributes = ['Pclass','Sex','Age','Fare','Survived']
     data = read_data('../titanic_data.csv',attributes)
@@ -33,7 +84,7 @@ def regression():
     x_list = []
     y_list = []
 
-    for x in range(int(len(data)*.7)):
+    for x in range(int(len(data) * .7)):
         y_list.append(float(data[x].get('Survived')))
         temp_list = []
         for y in attributes:
@@ -41,7 +92,7 @@ def regression():
         x_list.append(temp_list)
         temp_list = []
     lr = linear_model.LinearRegression()
-    lr.fit(x_list,y_list)
+    lr.fit(x_list, y_list)
     print('The coefficients are ' + str(lr.coef_))
 
     test_list = []
@@ -66,5 +117,6 @@ def regression():
             count += 1
     print("Percent correct " + str(count/thirty_percent))
 
-
+gradient_descent()
+print('\n')
 regression()
